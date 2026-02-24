506d87779ed3cba6ca8c6c9b33af5d5793bfd700
Per Sandberg <per.s.sandberg@bahnhof.se>
2026-02-24 13:14:57 +0100

    Restructure

---
+++ Ada/test/src/example.adb
@@ -0,0 +1,78 @@
+with Ada.Calendar;
+with Ada.Text_IO;
+with Ada.Long_Float_Text_IO;
+with GNAT.Altivec;
+with GNAT.Ctrl_C;
+with Interfaces.C;
+with Waveshare.ADS1263;
+with GNAT.Exception_Traces;
+with GNAT.Traceback.Symbolic;
+
+with bcm2835_h_Impl; -- to get the mockup
+
+package body example is
+   use Waveshare;
+   use Ada.Long_Float_Text_IO;
+   use Ada.Text_IO;
+   use type Interfaces.C.unsigned_long;
+   use all type Ada.Calendar.Time;
+   Continue : Boolean := True with Warnings => Off;
+
+   -- Modify REF according to actual voltage
+   -- External AVDD and AVSS(Default), or internal 2.5V
+   procedure On_Ctrl_C is
+   begin
+      Continue := False;
+   end;
+
+   procedure TEST_ADC1 is
+      Channels : constant ADS1263.Channel_List := (0, 1, 2, 3, 4);
+      Value    : ADS1263.Data_Values (Channels'Range);
+   begin
+      ADS1263.SetMode (0);
+      ADS1263.init_ADC1 (ADS1263.DRATE_400SPS);
+      while Continue loop
+         Put_Line ("--------------------------------------");
+         Value := ADS1263.Get_ADC1 (Channels);
+         for I in  Channels'range loop
+            Put ("Ch:" & Channels (I)'image & " =>" );
+            Put (Value (I)'image & "  ");
+            Put (Item => ADS1263.Scale (Value (I)));
+            New_Line;
+         end loop;
+         delay 1.0;
+      end loop;
+   end;
+
+   procedure TEST_ADC2 is
+      Value    : ADS1263.All_Data_Values;
+   begin
+      ADS1263.Init_ADC2 (ADS1263.ADC2_100SPS);
+      while Continue loop
+         ADS1263.GetAll_ADC2 (Value);
+         Put_Line (Value'image);
+      end loop;
+   end;
+
+   procedure TEST_ADC1_RATE is
+      Dummy    : Interfaces.C.Unsigned_Long;
+      start    : Ada.Calendar.Time;
+      Time     : Duration;
+      Times    : constant := 10000;
+   begin
+      start := Ada.Calendar.Clock;
+      for I in 1 .. Times loop
+         Dummy := ADS1263.GetChannalValue (0);
+      end loop;
+
+      Time := Ada.Calendar.Clock - Start;
+      Put_Line (Time'Image);
+   end;
+
+   procedure TEST_RTD is
+      Data    : Interfaces.C.Unsigned_Long;
+   begin
+      Data := ADS1263.RTD (ADS1263.DELAY_8d8ms, ADS1263.GAIN_1, ADS1263.DRATE_20SPS);
+
+   end;
+end example;
---
+++ Ada/test/src/example.ads
@@ -0,0 +1,8 @@
+package example is
+   REF      :  constant := 5.08;
+   procedure On_Ctrl_C;
+   procedure TEST_ADC1;
+   procedure TEST_ADC2;
+   procedure TEST_ADC1_RATE;
+   procedure TEST_RTD;
+end example;
